796553bdea33fc491fc92b1f589f4bf7d1cb197b
liuxiaobing <liuxiaobing@espacetime.com>
2017-09-08 10:12:34 +0800

    Initial Commit

---
+++ TableViewRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewRefresh
+//
+//  Created by 刘小兵 on 2017/9/8.
+//  Copyright © 2017年 刘小兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
